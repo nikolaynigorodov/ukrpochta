a0904785586792f2100bc168b56d93eb13bb1aed
Nikolay <nikolaynigorodov@gmail.com>
2022-07-01 17:15:32 +0300

    login and registration

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
     @if (Route::has('login'))
         <div class="hidden fixed px-6 py-4 sm:block">
             @auth
-                <a href="{{ route("logout") }}" class="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1">Log out</a>
+                <a href="{{ route("logout") }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Log out</a>
             @else
                 <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
