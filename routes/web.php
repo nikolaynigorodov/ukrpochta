794f2a00c889cb8d2e8f4a7cd80025f5cf660bc2
Nikolay <nikolaynigorodov@gmail.com>
2022-07-01 17:31:16 +0300

    login and registration small fix

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', MainController::class)->name('home');
+
 Route::middleware('auth')->group(function () {
     Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 });
 
 Route::middleware('guest')->group(function () {
-    Route::get('/', MainController::class)->name('home');
 
     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('/login_process', [LoginController::class, 'login'])->name('login_process');
