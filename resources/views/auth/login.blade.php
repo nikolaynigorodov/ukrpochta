fbf81ad8e3c635ef8ae8802fd326912519e46a2c
Nikolay <nikolaynigorodov@gmail.com>
2022-07-03 18:33:34 +0300

    view small fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     @include('partials.header')
-    <div class="container">
+    <div class="container mt-3">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,20 @@
-<div class="relative">
-    @if (Route::has('login'))
-        <div class="hidden fixed px-6 py-4 sm:block">
-            @auth
-                <a href="{{ route("logout") }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Log out</a>
-            @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav mx-auto">
+            @if (Route::has('login'))
+                @auth
+                    <a href="{{ route("logout") }}" class="nav-item nav-link" style="font-size: medium">Log out</a>
+                @else
+                    <a href="{{ route('login') }}" class="nav-item nav-link" style="font-size: medium">Log in</a>
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                @endif
-            @endauth
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="nav-item nav-link" style="font-size: medium">Registration</a>
+                    @endif
+                @endauth
+            @endif
         </div>
-    @endif
-</div>
+    </div>
+</nav>
